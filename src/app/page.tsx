177c52f8059b4280a64bcde54c34763a052806c1
Jorge E Espinoza <jorge.e.espinoza.57@gmail.com>
2025-04-29 20:50:10 -0500

    add some narcissism to the home page (very good)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,13 +12,10 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1 className="text-6xl">^app by Jorge Espinoza</h1>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+            <s>Add some narcissism to this app</s>
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
